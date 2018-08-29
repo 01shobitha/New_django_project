350c0d8a88ac5ea70669885ba13705757d35775e
Shobitha-M <shobithastar@gmail.com>
2018-08-29 22:26:31 +0530

    Slug

--- mysite/restaurants/models.py
+++ mysite/restaurants/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.db.models.signals import pre_save, post_save
+from .utils import unique_slug_generator
 class RestaurantLocation(models.Model):
     name            = models.CharField(max_length=120)
     location        = models.CharField(max_length=120, null=True, blank=True)
@@ -12,6 +12,20 @@ class RestaurantLocation(models.Model):
     #my_date_field   = models.DateField(auto_now=False, auto_now_add= False)
     def __str__(self):
         return self.name
+
     @property
     def title(self):
-        self.name
+        return self.name
+
+def rl_pre_save_reciever(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# def rl_post_save_reciever(sender, instance, created, *args, **kwargs):
+#     print('saved!')
+#     print(instance.timestamp)
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save()
+
+pre_save.connect(rl_pre_save_reciever, sender=RestaurantLocation)
