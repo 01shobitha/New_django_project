7c075412d96ad44217ec45a9241b4ab8c4c07eaa
Shobitha-M <shobithastar@gmail.com>
2018-08-31 23:15:18 +0530

    django forms

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,13 +24,15 @@ from restaurants.views import (
      restaurant_listview,
      RestaurantListView,
      RestaurantDetailView,
+     restaurants_createview
      )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^restaurants/$', RestaurantListView.as_view()),
-    url(r'^restaurants/(?P<rest_id>\w+)$', RestaurantDetailView.as_view()),
+    url(r'^restaurants/create/$',restaurants_createview),
+    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
     #url(r'^restaurants/asian/$', AsianFusionRestaurantListView.as_view()),
 
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
